7dff107a1a089c5b601054dae31ac30c51a79556
afurs <deliner2012@gmail.com>
2021-03-27 16:09:34 +0300

    New features for TriggerClass

--- src/trigger/TriggerClass.h
+++ src/trigger/TriggerClass.h
@@ -5,12 +5,16 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <regex>
 
 #include "TString.h"
 #include "TMap.h"
 #include "TObjString.h"
 #include "TTree.h"
+#include "TFile.h"
 
+#include "AnalysisUtils.h"
 struct TriggerClass {
   //Full trigger word;
   std::string mTriggerClassName;
@@ -22,17 +26,22 @@ struct TriggerClass {
   double mDownscaleFactor;
   unsigned long long mTriggerMask;
   unsigned long long mTriggerMaskNext50;
-  unsigned int mRunNum;
   bool checkTriggerMask(unsigned long long triggerMask,unsigned long long triggerMaskNext50=0)  const;
-  void print(bool csvFormat=false) const;
+  void print(bool csvFormat=false, unsigned int runnum=0) const;
 };
 struct TriggerClassManager {
+  TriggerClassManager(unsigned int runnum,std::string filepaths):mRunNum(runnum),mFilepath(filepaths){}
+  TriggerClassManager()=default;
+  unsigned int mRunNum;
+  std::string mFilepath;
   std::map<std::string,TriggerClass> mMapTrgNameToTrgClass;
   bool checkTriggerMask(std::string trgClassName,unsigned long long triggerMask,unsigned long long triggerMaskNext50=0) const;
-  void init(unsigned int runnum,TTree *treeTrgClasses);
+  bool defineTrgSum(std::string name, std::string title, std::vector<std::string> vecKeywords, bool keywordExact);
+  std::size_t defineTrgSumReg(std::string name, std::string title, std::string regExprSt);
+  void init();
+  void init(TTree *treeTrgClasses);
   void print(bool csvFormat=false) const;
   bool isReady() const;
-
 };
 
 #endif
