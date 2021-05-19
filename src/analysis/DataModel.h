c9454575a6891f12cac690cb9828f476c5b13390
afurs <afurs@cern.ch>
2021-05-19 18:51:12 +0200

    New types for Output manager

--- src/analysis/DataModel.h
+++ src/analysis/DataModel.h
@@ -1,5 +1,7 @@
 #ifndef DataModel_h
 #define DataModel_h
+#include "HelperHists.h"
+
 #include <string>
 #include <vector>
 #include <functional>
@@ -20,8 +22,22 @@ class DataOutputObject {
   typedef DataOutputType DataOutput_t;
   typedef std::function<void(const Data_t&,DataOutputObject_t *)> Functor_t;
   typedef typename EventCutID_t::template EventCutIDmap<unsigned int> MapEventCutIDtoBin_t;
+  
+//  template<typename = void >
+//  DataOutputObject(Data_t &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin):
+//    mRefData(data),mEventCutID(EventCutID_t{}) {}
+//  template<typename... Args>
+//  DataOutputObject(Args&&... args){}
+  
+//  template<typename = typename std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1 > >
+//  template<typename DataType = std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1,Data_t > >
+/*
+  template<typename DataT = int>
+  DataOutputObject(DataT &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin):
+    mRefData(data),mEventCutID(EventCutID_t{}) {}
 
-  DataOutputObject(Data_t &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin):
+  template<typename DataT = std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1,Data_t > >
+  DataOutputObject(DataT &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin):
     mRefData(data),mEventCutID(EventCutID_t{})
   {
     mIsEventCutIDCounter=true;
@@ -38,12 +54,25 @@ class DataOutputObject {
     }
     //mFuncDataFill = mFuncDataFillEventCutIDbins;
   }
+*/
   /*******************************************************************************************************************/
-  DataOutputObject(Data_t &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin,const EventCutID_t &eventCutID):
+//  template<typename=void>
+//  DataOutputObject(Data_t &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin,const EventCutID_t &eventCutID):
+//    mRefData(data),mEventCutID(EventCutID_t{}) {}
+
+
+//  template<typename = typename std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1 > >
+
+//  template<typename DataT = int>
+//  DataOutputObject(DataT &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin,const EventCutID_t &eventCutID):
+//    mRefData(data),mEventCutID(eventCutID){}
+
+//  template<typename DataT = std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1,Data_t> >
+  DataOutputObject(Data_t &data,std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin,const EventCutID_t eventCutID = {}):
     mRefData(data),mEventCutID(eventCutID)
   {
     mIsEventCutIDCounter=true;
-    mFuncDataFill = [](const Data_t& data,DataOutputObject_t *outputPtr)->void {outputPtr->fillData(data.mEventCutID);};
+/*    mFuncDataFill = [](const Data_t& data,DataOutputObject_t *outputPtr)->void {outputPtr->fillData(data.mEventCutID);};
     int nBin=1;
     for(const auto& entry: vecEventCutIDtoBin)  {
       auto [it, isNew] = mMapEventCutIDToBin.insert({entry,nBin});
@@ -54,6 +83,9 @@ class DataOutputObject {
     for(const auto& entry: mMapEventCutIDToBin) {
       mDataOutput.GetXaxis()->SetBinLabel(entry.second,entry.first.mEventCutIDname.c_str());
     }
+*/
+    prepareStatHist<DataOutputType>(name,title,vecEventCutIDtoBin);
+    
     //mFuncDataFill = mFuncDataFillEventCutIDbins;
   }
   /*******************************************************************************************************************/
@@ -67,6 +99,28 @@ class DataOutputObject {
   {
     mIsEventCutIDCounter=false;
   }
+  /*******************************************************************************************************************/
+  template<typename T,
+    typename std::enable_if_t<helpers::hists::HistHelper<T>::Ndims==1,bool> = true>
+  void prepareStatHist(std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin) {
+    mFuncDataFill = [](const Data_t& data,DataOutputObject_t *outputPtr)->void {outputPtr->fillData<T>(data.mEventCutID);};
+    int nBin=1;
+    for(const auto& entry: vecEventCutIDtoBin)  {
+      auto [it, isNew] = mMapEventCutIDToBin.insert({entry,nBin});
+      if(isNew) nBin++;
+    }
+    nBin--;
+    mDataOutput = DataOutput_t{name.c_str(),title.c_str(),nBin,0,static_cast<double>(nBin)};
+    for(const auto& entry: mMapEventCutIDToBin) {
+      mDataOutput.GetXaxis()->SetBinLabel(entry.second,entry.first.mEventCutIDname.c_str());
+    }
+  }
+
+//  template<typename std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims!=1,bool> = true>
+  template<typename T,
+      typename std::enable_if_t<helpers::hists::HistHelper<T>::Ndims!=1,bool> = true  >
+  void prepareStatHist(std::string name,std::string title,const std::vector<EventCutID_t> &vecEventCutIDtoBin) {}
+
   //Reference for data;
   std::reference_wrapper<Data_t> mRefData;
   //Reference for output;
@@ -90,8 +144,13 @@ class DataOutputObject {
 
 
   /*******************************************************************************************************************/
+  //template<typename = typename std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims!=1 > >
+  //void fillData(const EventCutID_t &eventCutID);
 
-  void fillData(const EventCutID_t &eventCutID) {
+  //template<typename = typename std::enable_if_t<helpers::hists::HistHelper<DataOutputType>::Ndims==1 > >
+  template<typename T>
+  auto fillData(const EventCutID_t &eventCutID)-> std::enable_if_t<helpers::hists::HistHelper<T>::Ndims==1 >
+  {
     for(const auto& entry: mMapEventCutIDToBin) {
       if(entry.first.isAcceptable(eventCutID)) {
         mDataOutput.Fill(mDataOutput.GetBinCenter(entry.second));
@@ -99,6 +158,8 @@ class DataOutputObject {
     }
   }
 
+  auto fillData(const EventCutID_t &eventCutID)-> void
+  {}
   /*******************************************************************************************************************/
 
   void fillData() {
